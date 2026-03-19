9c1446d6adc29e0ac7cabb0a7a86115be765eb1b
AndresNavarrom <andresmarintuprigci@gamil.com>
2026-03-18 19:31:52 -0500

    program

--- Patterns/State/ErrorStatus.cs
+++ Patterns/State/ErrorStatus.cs
@@ -1,8 +1,15 @@
+using UMLIoT.Core.Devices;
+
 namespace UMLIoT.Patterns.State;
 
 public class ErrorStatus : DeviceStatus
 {
-    public override void handleStatus()
+    public override void handleStatus(Device device)
     {
+        Console.WriteLine("El dispositivo tiene un error.");
+
+        Console.WriteLine("Revisando fallos...");
+
+        //device.setStatus(new OfflineStatus());
     }
 }
